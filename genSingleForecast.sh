c7180ec3edb0d8f94d17e4a5d1868719e8367a47
Nicholas G Reich <nick@schoolph.umass.edu>
2014-09-08 15:44:10 -0400

    date-based updates to make scripts.

--- genSingleForecast.sh
+++ genSingleForecast.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd ~/Documents/code_versioned/denguePrediction/dengueForecastPipeline/
-Rscript makeForecast.R 2014-09-07 > forecasts/20140907_forecast.Rout
+Rscript makeForecast.R 2014-09-07 > forecasts/20140908_forecast.Rout
