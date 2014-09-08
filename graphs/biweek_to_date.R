247d21a5afc0ea0570a0af96eff57f1c91d510f9
stephenalex913 <stephenalauer@gmail.com>
2014-09-08 13:54:25 -0400

    Made biweek_to_date faster

--- graphs/biweek_to_date.R
+++ graphs/biweek_to_date.R
@@ -1,29 +1,17 @@
 require(cruftery)
 require(lubridate)
 
-biweek_to_date <- function(biweek, year=NULL){
-  biweek_map <- with(environment(date_to_biweek), leap_year_map)
-  while(biweek>26 & !is.null(year)){
-    biweek <- biweek-26
-    year <- year+1
-  }
-  if(biweek>26 & is.null(year)){
-    stop("Biweek must be less than 27 or year must be specified.")
-  }
-  while(biweek<1 & !is.null(year)){
-    biweek <- biweek+26
-    year <- year-1
-  }
-  if(biweek<0 & is.null(year)){
-    stop("Biweek must be greater than 0 or year must be specified.")
-  }
-  biweek_day <- biweek_map$julian_day[match(biweek, biweek_map$biweek)]
-  if(!is.null(year)){
-    biweek_date <- as.Date(biweek_day-1, origin=paste0("2008-01-01"))
-    year(biweek_date) <- year
-    return(biweek_date)
-  }
-  biweek_date <- format(as.Date(biweek_day-1, origin="2008-01-01"), format="%m-%d")
-  return(biweek_date)
+biweek_to_date <- function(biweek, year){
+ if(length(biweek)!=length(year))
+  message("Note: the input biweeks and years are not of the same length.")
+ biweek.df <- data.frame(Biweek=biweek, Year=year)
+ biweek.df$Leap <- leap_year(biweek.df$Year)
+ biweek.df$Yr.Adj <- 0
+ biweek.df$Yr.Adj <- biweek.df$Year + floor((biweek.df$Biweek - 1)/26)
+ biweek.df$Bw.Adj <- biweek.df$Biweek - 26*floor((biweek.df$Biweek - 1)/26)
+ biweek_map <- with(environment(date_to_biweek), leap_year_map)
+ biweek.df$Bw.Day <- biweek_map$julian_day[match(biweek.df$Bw.Adj, biweek_map$biweek)]
+ biweek.df$Date <- as.Date(biweek.df$Bw.Day-1, origin=paste0("2008-01-01"))
+ year(biweek.df$Date) <- biweek.df$Yr.Adj
+ return(biweek.df$Date)
 }
-
