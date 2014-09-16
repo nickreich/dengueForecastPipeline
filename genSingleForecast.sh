18c7834baa7b71600c9635862d5e31573831719f
stephenalex913 <stephenalauer@gmail.com>
2014-09-16 13:53:18 -0400

    only retrieve data from biweeks following data dumps, not every biweek

--- genSingleForecast.sh
+++ genSingleForecast.sh
@@ -2,3 +2,4 @@
 cd ~/Documents/code_versioned/denguePrediction/dengueForecastPipeline/
 Rscript getCounts.R > counts/`/bin/date "+%Y%m%d"`_counts.Rout
 Rscript makeForecast.R > forecasts/`/bin/date "+%Y%m%d"`_forecast.Rout
+Rscript graphForecasts.R > forecasts/`/bin/date "+%Y%m%d"`_forecast.Rout
--- writeScript.R
+++ writeScript.R
@@ -1,4 +1,9 @@
-dateList <- read.csv(paste0("data/", as.character(Sys.Date(), "%Y%m%d"), "dateList.csv"))
+## get most recent dates
+dates.list <- list.files(path="data", pattern = "*.csv")
+dates.info <- file.info(paste0("data/", dates.list))
+recent.date <- which.max(dates.info$mtime)
+dateList <- read.csv(paste0("data/", dates.list[recent.date]))
+
 sink("doForecast.sh")
 for(i in 1:length(dateList[,2])){
  shell.code <- paste0("Rscript makeForecast.R ", dateList[i,2], " > forecasts/",
