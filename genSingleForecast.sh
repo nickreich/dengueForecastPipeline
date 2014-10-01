d672d4a3e226563ee8b2726a4767827539c2b063
Nicholas G Reich <nick@schoolph.umass.edu>
2014-10-01 14:39:51 -0400

    removing graphForecast call in genSingleForecast. Also adding explicit delivery date for "week 38" data.

--- genSingleForecast.sh
+++ genSingleForecast.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 cd ~/Documents/code_versioned/denguePrediction/dengueForecastPipeline/
-Rscript getCounts.R > counts/`/bin/date "+%Y%m%d"`_counts.Rout
-Rscript makeForecast.R > forecasts/`/bin/date "+%Y%m%d"`_forecast.Rout
-Rscript graphForecasts.R > forecasts/`/bin/date "+%Y%m%d"`_graph.Rout
+Rscript getCounts.R "2014-09-29" > counts/`/bin/date "+%Y%m%d"`_counts.Rout
+Rscript makeForecast.R "2014-09-29" > forecasts/`/bin/date "+%Y%m%d"`_forecast.Rout
