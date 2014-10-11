c3bd1ac8b61777d3b105a672f413ae2968d47664
Nicholas Reich <nick@schoolph.umass.edu>
2014-10-11 07:41:21 +0700

    adding grey

--- shiny/global.R
+++ shiny/global.R
@@ -32,4 +32,4 @@ map_min <- min(mapMaxdf$cpp, na.rm=T)
 
 ## set graph colors (special for colorblind people)
 cbbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", 
-                "#0072B2", "#D55E00", "#CC79A7")
+                "#0072B2", "#D55E00", "#CC79A7", "#cccccc")
